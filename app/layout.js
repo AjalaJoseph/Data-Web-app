385b55a84373e992c57988e2cb4f015f05b9a65d
AjalaJoseph <ajalaoluwafikayomi27@gmail.com>
2025-11-06 21:04:49 +0100

    Add class to body element in RootLayout

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-white">
         <Providers>
           {children}
           <div className="fixed bottom-10 right-5">
